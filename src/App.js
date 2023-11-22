62eab8a203735b666476583f48e227d2517fc8d8
Hamees <hamees1993@gmail.com>
2023-11-22 10:02:53 +0530

    tailwind css integrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-4xl mx-auto p-3 sm:p-6 min-h-screen flex flex-col items-center">
+      <div className="w-full rounded-lg shadow">
+        <h1>Todo App</h1>
+        <div>New task input area</div>
+        <div>to do list area</div>
+      </div>
     </div>
   );
 }
