3aa203b7dd79de9731afe63e7ee1304d90c7b2e3
Hamees <hamees1993@gmail.com>
2023-11-22 10:33:54 +0530

    basic layout is finished

--- src/App.js
+++ src/App.js
@@ -3,10 +3,148 @@ import "./App.css";
 function App() {
   return (
     <div className="max-w-4xl mx-auto p-3 sm:p-6 min-h-screen flex flex-col items-center">
-      <div className="w-full rounded-lg shadow">
-        <h1>Todo App</h1>
-        <div>New task input area</div>
-        <div>to do list area</div>
+      <div className="w-full rounded-lg shadow-md p-4 border">
+        <h1 className="text-gray-700 text-3xl text-center font-bold">
+          Todo App
+        </h1>
+        <div className="mt-5">
+          <form className="flex items-center">
+            <div className="relative w-full">
+              <input
+                type="text"
+                id="simple-search"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="New Task..."
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="flex items-center gap-2 p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              <svg
+                class="w-3 h-3 text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 1v16M1 9h16"
+                />
+              </svg>
+              <span>Add</span>
+            </button>
+          </form>
+        </div>
+        <div className="mt-5">
+          <ul className="space-y-2">
+            <li className="flex justify-between items-center bg-gray-100 border shadow rounded p-2.5 px-3">
+              <div class="flex items-center">
+                <input
+                  id="link-checkbox1"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  checked
+                />
+                <label
+                  for="link-checkbox1"
+                  class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
+                >
+                  I agree with the.
+                </label>
+              </div>
+              <button type="button">
+                <svg
+                  class="w-5 h-5 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+                  />
+                </svg>
+              </button>
+            </li>
+            <li className="flex justify-between items-center bg-gray-100 border shadow rounded p-2.5 px-3">
+              <div class="flex items-center">
+                <input
+                  id="link-checkbox2"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="link-checkbox2"
+                  class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  I agree with the.
+                </label>
+              </div>
+              <button type="button">
+                <svg
+                  class="w-5 h-5 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+                  />
+                </svg>
+              </button>
+            </li>
+            <li className="flex justify-between items-center bg-gray-100 border shadow rounded p-2.5 px-3">
+              <div class="flex items-center">
+                <input
+                  id="link-checkbox3"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="link-checkbox3"
+                  class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  I agree with the.
+                </label>
+              </div>
+              <button type="button">
+                <svg
+                  class="w-5 h-5 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+                  />
+                </svg>
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
